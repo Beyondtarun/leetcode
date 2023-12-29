30d83453d9d93d4ad5ff2290be446403512535d1
Beyondtarun <teg.tarun@gmail.com>
2023-12-29 20:17:25 +0530

    0 ms solution

--- 114.cpp
+++ 114.cpp
@@ -9,7 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution{
 public:
 void preorder(TreeNode* root,vector<int> &ans){
          if(root==NULL)return;
