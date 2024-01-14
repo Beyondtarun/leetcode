1e90f7099f8138f3c7f56d983da8e1b36d4da20b
Beyondtarun <teg.tarun@gmail.com>
2024-01-14 11:31:07 +0530

    .

--- 1768.cpp
+++ 1768.cpp
@@ -14,3 +14,19 @@ public:
     return result;
     }
 };
+
+/*
+var mergeAlternately = function(word1, word2) {
+    let res ="";
+    let i =0
+    while(i<word1.length || i<word2.length){
+        if(i<word1.length)
+        res += word1[i];
+        if(i<word2.length)
+        res+= word2[i];
+
+        i++;
+    }
+    return res;
+};
+*/
